b96d54769adbc27daaafb5effc28ef051d03f50a
David P <david.perli2@gmail.com>
2025-01-30 11:39:53 +1000

    Authentication - Bypass (Google OAuth issue)

--- app/account/page.js
+++ app/account/page.js
@@ -1,13 +1,22 @@
-import Navigation from "../_components/Navigation";
+import { auth } from "../_lib/auth";
 
 export const metadata = {
-  title: "Account",
+  title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  let firstName;
+  if (session) {
+    firstName = session.user.name.split(" ").at(0);
+  } else {
+    firstName = "Guest User";
+  }
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Jonas
+      Welcome, {firstName}
     </h2>
   );
 }
